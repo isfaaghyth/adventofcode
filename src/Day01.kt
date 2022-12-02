5c19ec5a3b5a9b2f0a7ffae781190868372dba77
isfaaghyth <isfaaghyth@gmail.com>
2022-12-02 09:57:50 +0700

    Feat: day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,40 @@
 fun main() {
+    fun day01(input: List<String>): List<Int> {
+        val highers = mutableListOf<Int>()
+        var higher = 0
+        var temp = 0
+        for (i in input) {
+            if (i.isNotBlank()) {
+                temp += i.toInt()
+            } else {
+                if (temp > higher) {
+                    higher = temp
+                    highers.add(higher)
+                } else {
+                    higher = 0
+                }
+
+                temp = 0
+            }
+        }
+
+        return highers
+    }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        val result = day01(input)
+        return result.max()
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val result = day01(input)
+        return result
+            .sortedDescending()
+            .take(3) // top three
+            .sum()
     }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
-    val input = readInput("Day01")
+    val input = readInput("day01")
     println(part1(input))
     println(part2(input))
 }
